18c30323651ac99ccdbad08b240e1345d6b97a0f
George Coe <gd.coe@btinternet.com>
2014-11-01 14:13:27 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Snorpies::Application.config.secret_key_base = 'b05adcb2ca95d95c4eb1e067f4f724e52158c3118d5261f9ce7f69091c990d90a61641d798983f2425b4a846477b529145e0ee36fef940d5cad38bd8a4e01f9f'
