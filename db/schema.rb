01e335037cb0b092d78ce52228c024f8d5e4c280
George Coe <gd.coe@btinternet.com>
2014-11-04 21:01:56 -0500

    users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141102201425) do
+ActiveRecord::Schema.define(version: 20141105013748) do
 
   create_table "installs", force: true do |t|
     t.string   "email",                  default: "", null: false
